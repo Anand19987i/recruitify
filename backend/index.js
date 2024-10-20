163fefecb3ab32ff350bc215a8ae03dc0c44648b
Anand19987i <anandpandey1765@gmail.com>
2024-10-20 17:33:46 +0530

    Updated

--- backend/index.js
+++ backend/index.js
@@ -16,8 +16,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
+
 const corsOptions = {
-    origin:'https://recruitifyyy.vercel.app',
+    // origin:'https://recruitifyyy.vercel.app',
+    origin: 'http://localhost:5173',
     credentials:true
 }
 
--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -4,3 +4,9 @@ export const USER_API_END_POINT = `${BASE_URL}/user`;
 export const JOB_API_END_POINT = `${BASE_URL}/job`;
 export const APPLICATION_API_END_POINT = `${BASE_URL}/application`;
 export const COMPANY_API_END_POINT = `${BASE_URL}/company`;
+
+
+// export const USER_API_END_POINT = '/api/v1/user';
+// export const JOB_API_END_POINT = 'api/v1/job';
+// export const APPLICATION_API_END_POINT = '/api/v1/application';
+// export const COMPANY_API_END_POINT = '/api/v1/company';
