2b25a25ff6a3d8c46cfa20f8cece6c17dc7c2c1c
Anand19987i <anandpandey1765@gmail.com>
2024-10-20 15:54:46 +0530

    Updated

--- backend/index.js
+++ backend/index.js
@@ -17,15 +17,13 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'*',
+    origin:'https://recruitify-kxmv.vercel.app',
     credentials:true
 }
 
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
-// https://recruitify-kxmv.vercel.app
-
 // api's
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
