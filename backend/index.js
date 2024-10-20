4d2718764121e1a7bbee1a8caa4fdc55327028fa
Anand19987i <anandpandey1765@gmail.com>
2024-10-20 15:27:20 +0530

    Updated

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
     origin:'https://recruitify-kxmv.vercel.app',
+    credentials:true
 }
 
 app.use(cors(corsOptions));
