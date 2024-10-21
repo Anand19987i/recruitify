33d727342c0d2c37ec1c5ee081573736d07fc980
Anand19987i <114969806+Anand19987i@users.noreply.github.com>
2024-10-21 10:11:33 +0530

    Update authSlice.js

--- frontend/src/redux/authSlice.js
+++ frontend/src/redux/authSlice.js
@@ -1,20 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const authSlice = createSlice({
-    name:"auth",
-    initialState:{
-        loading:false,
-        user:null
+  name: "auth",
+  initialState: {
+    loading: false,
+    user: null,
+    token: null, // Add token state
+  },
+  reducers: {
+    setLoading: (state, action) => {
+      state.loading = action.payload;
     },
-    reducers:{
-        // actions
-        setLoading:(state, action) => {
-            state.loading = action.payload;
-        },
-        setUser:(state, action) => {
-            state.user = action.payload;
-        }
-    }
+    setUser: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token; // Store token in state
+    },
+    logout: (state) => {
+      state.loading = false;
+      state.user = null;
+      state.token = null;
+    },
+  },
 });
-export const {setLoading, setUser} = authSlice.actions;
+
+export const { setLoading, setUser, logout } = authSlice.actions;
 export default authSlice.reducer;
