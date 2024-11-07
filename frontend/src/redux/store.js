82c1f9c1ccabd53cdfc70a5f20381c49ca9640da
Anand <114969806+Anand19987i@users.noreply.github.com>
2024-11-07 21:23:33 +0530

    Update store.js

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,6 +1,8 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import authSlice from "./authSlice";
 import jobSlice from "./jobSlice";
+import companySlice from "./companySlice";
+import applicationSlice from "./applicationSlice";
 import {
     persistStore,
     persistReducer,
@@ -10,27 +12,28 @@ import {
     PERSIST,
     PURGE,
     REGISTER,
-} from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
-import companySlice from "./companySlice";
-import applicationSlice from "./applicationSlice";
+} from 'redux-persist';
+import storageSession from 'redux-persist/lib/storage/session'; // Use sessionStorage for persistence
 
+// Persist configuration
 const persistConfig = {
     key: 'root',
     version: 1,
-    storage,
-}
+    storage: storageSession,  // Using sessionStorage for persistence (cleared on tab close)
+};
 
+// Combine reducers from different slices
 const rootReducer = combineReducers({
-    auth:authSlice,
-    job:jobSlice,
-    company:companySlice,
-    application:applicationSlice
-})
+    auth: authSlice,
+    job: jobSlice,
+    company: companySlice,
+    application: applicationSlice,
+});
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+// Create persisted reducer using persistReducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-
+// Configure store
 const store = configureStore({
     reducer: persistedReducer,
     middleware: (getDefaultMiddleware) =>
@@ -40,4 +43,6 @@ const store = configureStore({
             },
         }),
 });
+
+// Export the store
 export default store;
