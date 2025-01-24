226d2ad881e0baf7a4a876b0c71850379f20d2ac
Anand19987i <anandpandey1765@gmail.com>
2025-01-24 16:07:11 +0530

    Committed

--- backend/routes/company.route.js
+++ backend/routes/company.route.js
@@ -5,15 +5,9 @@ import { singleUpload } from "../middlewares/multer.js";
 
 const router = express.Router();
 
-<<<<<<< HEAD
 router.route("/register/:userId").post(registerCompany);
 router.route("/get/:userId").get( getCompany);
 router.route("/get/:id").get(getCompanyById);
-=======
-router.route("/register").post(registerCompany);
-router.route("/get").get(getCompany);
-router.route("/get/:id").get(  getCompanyById);
->>>>>>> bf8bb31a862e6643cc03d1db27ac85dd554a0117
 router.route("/update/:id").put(singleUpload, updateCompany);
 
 export default router;
--- backend/routes/job.route.js
+++ backend/routes/job.route.js
@@ -6,18 +6,12 @@ const router = express.Router();
 
 router.route("/post").post(postJob);
 router.route("/get").get(getAllJobs);
-<<<<<<< HEAD
 router.route("/getadminjobs").post(getAdminJobs);
 router.route("/get/:id").get(getJobById);
 router.route("/update/:id").put(updateJob);
 router.route("/filter-jobs").get(filterCard);
 router.route("/category").get(categoryJobs);
 
-=======
-router.route("/getadminjobs").get(getAdminJobs);
-router.route("/get/:id").get(getJobById);
-router.route("/update/:id").put(updateJob);
->>>>>>> bf8bb31a862e6643cc03d1db27ac85dd554a0117
 
 
 export default router;
